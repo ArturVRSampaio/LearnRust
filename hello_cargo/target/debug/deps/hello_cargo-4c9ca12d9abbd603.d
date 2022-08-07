4c55c9883db199d8bc99b00bf85c2454c74a1128
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2022-08-07 06:00:54 -0300

    cargo example

---
+++ hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d
@@ -0,0 +1,5 @@
+/home/bypass/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.rmeta: src/main.rs
+
+/home/bypass/Desktop/rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bypass/Desktop/rust/hello_cargo/target/debug/hello_cargo: /home/bypass/Desktop/rust/hello_cargo/src/main.rs
